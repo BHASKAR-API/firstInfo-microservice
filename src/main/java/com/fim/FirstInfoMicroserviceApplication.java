9ed3aa8ca7f44b73fbbbedee5a429459d1568d4e
Bhaskara <bhaskara9492@gmail.com>
2021-10-09 13:22:39 +0530

    Intial Commit

---
+++ src/main/java/com/fim/FirstInfoMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.fim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstInfoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Application Started.......");
+		SpringApplication.run(FirstInfoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fim/FirstInfoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstInfoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
