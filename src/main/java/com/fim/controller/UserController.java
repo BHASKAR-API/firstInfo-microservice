e181c8b4a4df97ba693a3c6bf80f261541e2c84b
Bhaskara <bhaskara9492@gmail.com>
2021-10-14 13:14:35 +0530

    file added

---
+++ src/main/java/com/fim/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.fim.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("/get")
+	public String heart() {
+		return "Application Started Successfully.............";
+		
+	}
+
+}
