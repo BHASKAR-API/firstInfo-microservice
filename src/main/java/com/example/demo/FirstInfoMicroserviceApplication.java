9de1f874981bda326f9483e70040c06fcd4e863c
Venkat <parisapoguvenkat@gmail.com>
2021-10-16 11:21:00 +0530

    initial commit by Venkat

---
+++ src/main/java/com/example/demo/FirstInfoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstInfoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstInfoMicroserviceApplication.class, args);
+	}
+
+}
