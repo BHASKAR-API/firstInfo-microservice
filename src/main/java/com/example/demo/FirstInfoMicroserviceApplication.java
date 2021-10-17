3b8c8de88f5afdd69184cb907a538a50edb49e91
Venkat <parisapoguvenkat@gmail.com>
2021-10-17 16:30:53 +0530

    component scan added

--- src/main/java/com/example/demo/FirstInfoMicroserviceApplication.java
+++ src/main/java/com/example/demo/FirstInfoMicroserviceApplication.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
 public class FirstInfoMicroserviceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Added");
 		SpringApplication.run(FirstInfoMicroserviceApplication.class, args);
 	}
 
