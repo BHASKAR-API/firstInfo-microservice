bb1962ade3343e86f1455c1b4d0bfff4ae8ac1b8
Venkat <parisapoguvenkat@gmail.com>
2021-10-17 17:18:20 +0530

    Reverted by Venkat Revert "...Reverted...Revert "modified""
    This reverts commit d21f64a4eaae844b2134d3aeda9e4b089fc8bd14.

--- src/main/java/com/example/demo/FirstInfoMicroserviceApplication.java
+++ src/main/java/com/example/demo/FirstInfoMicroserviceApplication.java
@@ -8,6 +8,7 @@ public class FirstInfoMicroserviceApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Added");
+		System.out.println("Modified");
 		SpringApplication.run(FirstInfoMicroserviceApplication.class, args);
 	}
 
